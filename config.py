71365a517356ee859b084d77430824f0022c87f8
khainee <118953358+khainee@users.noreply.github.com>
2023-02-06 17:41:30 +0630

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+import logging
+class Config:
+    API_ID = int(os.environ.get("API_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")       
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    Drivebuzz_crypt = os.environ.get("Drivebuzz_crypt", "")
+    Drivefire_crypt = os.environ.get("Drivefire_crypt", "")
+    Jiodrive_crypt = os.environ.get("Jiodrive_crypt", "")
+    Gadrive_crypt = os.environ.get("Gadrive_crypt", "")
+    Gadrive_crypt = os.environ.get("Gadrive_crypt", "")
+    Katdrive_crypt = os.environ.get("Katdrive_crypt", "")
+    Gtot_crypt = os.environ.get("Gtot_crypt", "")
+    Appdrive_email = os.environ.get("Appdrive_email", "")
+    Appdrive_password = os.environ.get("Appdrive_password", "")
+    Hubdrive_crypt = os.environ.get("Hubdrive_crypt", "")
+    Sharerpw_xsrf_token = os.environ.get("Sharerpw_xsrf_token", "")
+    Sharerpw_laravel_session = os.environ.get("Sharerpw_laravel_session", "")
